9e4ccb90af2ab48028aff75dd8f95f618afe051d
Jeff Mesnil <jmesnil@gmail.com>
2010-11-27 19:58:01 +0100

    Warn if DeviceOrientation Event is not supported
    * add a warning below the canvas when the web browser does not support
      the DeviceOrientation Event API. The browser can still display the
      moving pieces of the other users (broadcasted by the server)

--- client.js
+++ client.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+   if(!window.DeviceMotionEvent) {
+      var msg = "Your Web Browser does not support the DeviceOrientation Event API.<br>";
+      msg +=    "You will be able to see the pieces of other users moving but do not try to tilt your screen :)"
+      $("#warning").html(msg);
+   }
+
    window.addEventListener("devicemotion", function(event) {
       var accel = event.accelerationIncludingGravity;
       Board.updateCenter(accel);
